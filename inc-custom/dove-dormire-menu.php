076bf00e7bf1d496c645a1ff6e524f3d485ed51f
serii <seriiburduja@gmail.com>
2020-02-27 11:50:35 +0200

    dove-dormire-mobile-menu links

--- inc-custom/dove-dormire-menu.php
+++ inc-custom/dove-dormire-menu.php
@@ -1,18 +1,27 @@
-<li class="main-menu__item"><a class="main-menu__link" href="<?php echo home_url(); ?>/rosolina-dove-dormire/">Dove dormire<img class="menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/i/menu-icon.svg" alt="icon"></a>
-	<div class="sub-menu">
-		<div class="sub-menu__wrap">
-			<div class="sub-menu__content">
-				<h3 class="sub-menu__title">Dove dormire</h3>
-				<p class="sub-menu__text">Rosolina Mare è famosa per le sue spiagge incantevoli e per i suoi stabilimenti balneari adatti a grandi e piccoli.</p>
-			</div>
-			<ul class="sub-menu__list">
-				<li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/hotel/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-5.jpg">
-						<h4 class="sub-menu__item-title">Hotel</h4></a></li>
-				<li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/villaggi-turistici/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-6.jpg">
-						<h4 class="sub-menu__item-title">Villaggi turistici</h4></a></li>
-				<li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/agenzie-immobiliari/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-7.jpg">
-						<h4 class="sub-menu__item-title">Agenzie immobiliari</h4></a></li>
-			</ul>
-		</div>
-	</div>
+<li class="main-menu__item">
+    <a class="main-menu__link"
+       href="<?php echo home_url(); ?>/rosolina-dove-dormire/">Dove dormire
+        <img class="menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/i/menu-icon.svg" alt="icon">
+    </a>
+    <div class="sub-menu">
+        <div class="sub-menu__wrap">
+            <div class="sub-menu__content">
+                <h3 class="sub-menu__title">Dove dormire</h3>
+                <p class="sub-menu__text">Rosolina Mare è famosa per le sue spiagge incantevoli e per i suoi stabilimenti balneari adatti a grandi e piccoli.</p>
+            </div>
+            <ul class="sub-menu__list">
+                <li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/hotel/">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-5.jpg">
+                        <h4 class="sub-menu__item-title">Hotel</h4></a></li>
+                <li class="sub-menu__item"><a
+                            href="<?php echo home_url(); ?>/rosolina-dove-dormire/villaggi-turistici/">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-6.jpg">
+                        <h4 class="sub-menu__item-title">Villaggi turistici</h4></a></li>
+                <li class="sub-menu__item"><a
+                            href="<?php echo home_url(); ?>/rosolina-dove-dormire/agenzie-immobiliari/">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-7.jpg">
+                        <h4 class="sub-menu__item-title">Agenzie immobiliari</h4></a></li>
+            </ul>
+        </div>
+    </div>
 </li>
--- inc-custom/dove-dormire-mobile-menu.php
+++ inc-custom/dove-dormire-mobile-menu.php
@@ -1,8 +1,14 @@
 <li class="main-menu-mobile__item">
-	<div class="main-menu-mobile__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/arrow-down-white.svg" alt="arrow"></div><a class="main-menu-mobile__link" href="https://visitrosolina.it/dove-dormire/">Dove dormire</a>
-	<ul class="main-menu-mobile-submenu">
-		<li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link" href="hotel.html">Hotel</a></li>
-		<li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link" href="vilagi-turistici.html">Villaggi turistici</a></li>
-		<li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link" href="agenzie-immobiliari.html">Agenzie immobiliari</a></li>
-	</ul>
+    <div class="main-menu-mobile__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/i/arrow-down-white.svg" alt="arrow">
+    </div>
+    <a class="main-menu-mobile__link" href="<?php echo home_url(); ?>/rosolina-dove-dormire/">Dove dormire</a>
+    <ul class="main-menu-mobile-submenu">
+        <li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link" href="<?php echo home_url(); ?>/rosolina-dove-dormire/hotel/">Hotel</a>
+        </li>
+        <li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link"
+                                                      href="<?php echo home_url(); ?>/rosolina-dove-dormire/villaggi-turistici/">Villaggi turistici</a></li>
+        <li class="main-menu-mobile-submenu__item"><a class="main-menu-mobile-submenu__link"
+                                                      href="<?php echo home_url(); ?>/rosolina-dove-dormire/agenzie-immobiliari/">Agenzie immobiliari</a></li>
+    </ul>
 </li>
