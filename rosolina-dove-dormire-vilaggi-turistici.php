16fa819311bf23336148826284807e5d5bc7287c
serii <seriiburduja@gmail.com>
2020-02-27 10:10:33 +0200

    menu in another file

--- rosolina-dove-dormire-vilaggi-turistici.php
+++ rosolina-dove-dormire-vilaggi-turistici.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Template Name: Rosolina Dove Dormire Vilaggi Turistici
- * Template Post Type: post
  */
 ?>
 
@@ -77,24 +76,7 @@
                 </div>
             </div>
         </li>
-        <li class="main-menu__item"><a class="main-menu__link" href="<?php echo home_url(); ?>/rosolina-dove-dormire/">Dove dormire<img class="menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/i/menu-icon.svg" alt="icon"></a>
-            <div class="sub-menu">
-                <div class="sub-menu__wrap">
-                    <div class="sub-menu__content">
-                        <h3 class="sub-menu__title">Dove dormire</h3>
-                        <p class="sub-menu__text">Rosolina Mare è famosa per le sue spiagge incantevoli e per i suoi stabilimenti balneari adatti a grandi e piccoli.</p>
-                    </div>
-                    <ul class="sub-menu__list">
-                        <li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/hotel/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-5.jpg">
-                                <h4 class="sub-menu__item-title">Hotel</h4></a></li>
-                        <li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/vilaggi-turistici/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-6.jpg">
-                                <h4 class="sub-menu__item-title">Villaggi turistici</h4></a></li>
-                        <li class="sub-menu__item"><a href="<?php echo home_url(); ?>/rosolina-dove-dormire/agenzie-immobiliari/"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/sub-menu-7.jpg">
-                                <h4 class="sub-menu__item-title">Agenzie immobiliari</h4></a></li>
-                    </ul>
-                </div>
-            </div>
-        </li>
+	    <?php require_once __DIR__.'/inc-custom/dove-dormire-menu.php';?>
         <li class="main-menu__item"><a class="main-menu__link"
                                        href="https://visitrosolina.it/webcam-e-meteo/">Webcam e meteo
                 <img class="menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/i/menu-icon.svg" alt="icon">
